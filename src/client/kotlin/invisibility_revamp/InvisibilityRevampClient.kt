66393649b54c576195290fd8be6b2fd40ccadd36
ProgrammerKid <23121997doa@gmail.com>
2025-05-24 17:53:27 +0500

    initial

---
+++ src/client/kotlin/invisibility_revamp/InvisibilityRevampClient.kt
@@ -0,0 +1,9 @@
+package invisibility_revamp
+
+import net.fabricmc.api.ClientModInitializer
+
+object InvisibilityRevampClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
